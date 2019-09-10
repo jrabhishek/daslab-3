b3b235c340abde33cedd7026ff42830e0317b3d5
Abhishek Raj <1828043@kiit.ac.in>
2019-09-10 19:15:22 +0530

    Add files via upload

---
+++ Ecell/app/src/main/java/com/ecell/android/ecell/Model.java
@@ -0,0 +1,13 @@
+package com.ecell.android.ecell;
+
+public class Model {
+    String s;
+
+    public String getS() {
+        return s;
+    }
+
+    public Model(String s) {
+        this.s = s;
+    }
+}
